3d867b2248f9a1353471243adc041d8d75e7ac98
AS30Cami <167490159+AS30Cami@users.noreply.github.com>
2024-05-06 21:54:22 -0400

    traspasando (no se había guardado)

---
+++ script.js
@@ -0,0 +1,13 @@
+const TrabajosRealizados = [
+{
+    Id: 2560349,
+    Titulo: "Cambio de aceite",
+    Descripcion: "Realizado el cambio de aceite y filtro de un vehículo Kia Rio.",
+    Mecánico: "Lucas Armijo",
+    Fecha: "2023-28-12"
+    Materiales: ["Aceite de motor", "Filtro de aceite"],
+    Imagenes: ["Cambio_aceite.jpg", "Cambio_aceite2.jpg"]
+
+} ,
+
+];
